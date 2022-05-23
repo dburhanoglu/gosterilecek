9a326d1dc5f7f97b472b2c6f1c5060a8df021850
dburhanoglu <84340536+dburhanoglu@users.noreply.github.com>
2022-05-23 19:14:53 +0300

    goster

---
+++ Gosterilecek/routes/posts.js
@@ -0,0 +1,45 @@
+const express = require('express')
+const router = express.Router()
+const Post = require(`../models/Post`)
+const path = require('path')
+const Category = require('../models/Category')
+
+
+router.get('/new',(req,res)=>{
+    if(!req.session.userId){
+     res.redirect('users/login')
+    }
+
+Category.find({}).lean().then(categories=> {
+  res.render('site/addpost',{categories:categories}) 
+})
+})
+
+router.post(`/test`, (req, res) => {
+
+
+    let post_image = req.files.post_image
+    post_image.mv(path.resolve(__dirname, '../public/img/postimages', post_image.name))
+
+
+    Post.create({
+        ...req.body,
+        post_image: `/img/postimages/${post_image.name}`
+    })
+
+    req.session.sessionFlash = {
+        type: 'alert alert-success',
+        message: 'postunuzz olusturuldu'
+    }
+
+    res.redirect('/blog')
+
+})
+router.get(`/:id`, (req, res) => {
+    Post.findById(req.params.id).lean().then(post => {
+        res.render('site/post', { post: post })
+    })
+
+})
+
+module.exports = router
